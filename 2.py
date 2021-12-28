33207290efe0f8e9bf3d65802a06425175954de4
ramseshu1 <49634683+ramseshu1@users.noreply.github.com>
2021-12-28 14:41:31 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,31 @@
+"""
+for x in range(0,100):
+    if x>10:
+        print("Forever in England")
+    elif x<5:
+        print("Jurisdiction")
+    else:
+        print("Object")
+
+i = 'turtle'
+if i is 'turtle':
+    print(i)
+
+latch = 5
+while(latch < 10):
+    print(latch)
+    latch += 1
+
+n=1
+
+if n is 1:
+    print(n, "is the magic number!")
+
+"""
+i=1
+j=10
+
+while(i<11 & j>0):
+    print('*'*i,'+'*j)
+    i=i+1
+    j=j-1
